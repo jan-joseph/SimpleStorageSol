c7b1ab08aa631087e237104713b7d57ed31b4f94
Jan Joseph <jan00joseph@gmail.com>
2021-11-13 16:06:24 +0530

    Final Modification
    Contract Transaction Example Handled

--- deploy.py
+++ deploy.py
@@ -1,6 +1,10 @@
 from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -37,9 +41,9 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # for Connecting to Ganache
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
-print(w3)
 chain_id = 1337
-my_address = "0x25579e6d8AdABb6746E68211e45DA954261f27c6"
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in Python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -53,4 +57,30 @@ transaction = SimpleStorage.constructor().buildTransaction(
 )
 
 # Signing the Transaction
-signed_txn = w3.eth.account.
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# Send the transaction
+print("Deploying contract")
+txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Block Confirmation
+txn_receipt = w3.eth.wait_for_transaction_receipt(txn_hash)
+print("Deployed")
+
+
+# Working with the Contract
+simple_storage = w3.eth.contract(address=txn_receipt.contractAddress, abi=abi)
+print(simple_storage.functions.retrieve().call())
+
+# Running a Transact Function
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+print("Updating Contract")
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+store_txn_hash = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+txn_receipt = w3.eth.wait_for_transaction_receipt(store_txn_hash)
+print("Updated!")
+print(simple_storage.functions.retrieve().call())
+
